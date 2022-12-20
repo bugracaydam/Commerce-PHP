d781ece29f344143ab6eccc3dec43746678ef6e3
Buğra <106000933+bugracaydam@users.noreply.github.com>
2022-12-20 16:25:04 +0300

    Add files via upload

---
+++ register.php
@@ -0,0 +1,163 @@
+﻿<?php include 'header.php';
+if (isset($_POST['kullanicikaydet'])) {
+
+	
+	echo $kullanici_adsoyad=htmlspecialchars($_POST['kullanici_adsoyad']); echo "<br>";
+	echo $kullanici_mail=htmlspecialchars($_POST['kullanici_mail']); echo "<br>";
+
+	echo $kullanici_passwordone=trim($_POST['kullanici_passwordone']); echo "<br>";
+	echo $kullanici_passwordtwo=trim($_POST['kullanici_passwordtwo']); echo "<br>";
+
+
+
+	if ($kullanici_passwordone==$kullanici_passwordtwo) {
+
+
+		if (strlen($kullanici_passwordone)>=6) {
+
+
+			
+
+			
+
+
+// Başlangıç
+
+			$kullanicisor=$db->prepare("select * from kullanici where kullanici_mail=:mail");
+			$kullanicisor->execute(array(
+				'mail' => $kullanici_mail
+			));
+
+			//dönen satır sayısını belirtir
+			$say=$kullanicisor->rowCount();
+
+
+
+			if ($say==0) {
+
+				//md5 fonksiyonu şifreyi md5 şifreli hale getirir.
+				$password=md5($kullanici_passwordone);
+
+				$kullanici_yetki=1;
+
+			//Kullanıcı kayıt işlemi yapılıyor...
+				$kullanicikaydet=$db->prepare("INSERT INTO kullanici SET
+					kullanici_adsoyad=:kullanici_adsoyad,
+					kullanici_mail=:kullanici_mail,
+					kullanici_password=:kullanici_password,
+					kullanici_yetki=:kullanici_yetki
+					");
+				$insert=$kullanicikaydet->execute(array(
+					'kullanici_adsoyad' => $kullanici_adsoyad,
+					'kullanici_mail' => $kullanici_mail,
+					'kullanici_password' => $password,
+					'kullanici_yetki' => $kullanici_yetki
+				));
+
+				if ($insert) {
+
+
+					header("Location:index.php?durum=loginbasarili");
+
+
+				//Header("Location:../production/genel-ayarlar.php?durum=ok");
+
+				} else {
+
+
+					header("Location:register.php?durum=basarisiz");
+				}
+
+			} else {
+
+				header("Location:register.php?durum=mukerrerkayit");
+
+
+
+			}
+
+
+
+
+		// Bitiş
+
+
+
+		} else {
+
+
+			header("Location:register.php?durum=eksiksifre");
+
+
+		}
+
+
+
+	} else {
+
+
+
+		header("Location:register.php?durum=farklisifre");
+	}
+	
+
+
+}
+ ?>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="page-title-wrap">
+				<div class="page-title-inner">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="bigtitle">Kullanıcı Kaydı</div>
+							<p >Kullanıcı kayıt işlemlerini aşağıda ki form aracılığı ile gerçekleştirebilirsiniz.</p>
+						</div>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<form action="" method="POST" class="form-horizontal checkout" role="form">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="title-bg">
+					<div class="title">Kayıt Bilgileri</div> 
+				</div>
+
+				<div class="form-group dob">
+					<div class="col-sm-12">
+						
+						<input type="text" class="form-control"  required="" name="kullanici_adsoyad" placeholder="Ad ve Soyadınızı Giriniz...">
+					</div>
+					
+				</div>
+				<div class="form-group">
+					<div class="col-sm-12">
+						<input type="email" class="form-control" required="" name="kullanici_mail"  placeholder="Dikkat! Mail adresiniz kullanıcı adınız olacaktır.">
+					</div>
+				</div>
+				<div class="form-group dob">
+					<div class="col-sm-6">
+						<input type="password" class="form-control" name="kullanici_passwordone"    placeholder="Şifrenizi Giriniz...">
+					</div>
+					<div class="col-sm-6">
+						<input type="password" class="form-control" name="kullanici_passwordtwo"   placeholder="Şifrenizi Tekrar Giriniz...">
+					</div>
+				</div>
+
+
+
+				<button type="submit" name="kullanicikaydet" class="btn btn-default btn-red">Kayıt İşlemini Yap</button>
+			</div>
+		</div>
+	</div>
+</form>
+<div class="spacer"></div>
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ sepet.php
@@ -0,0 +1,120 @@
+﻿<?php include 'header.php' ?>
+
+<div class="container">
+
+	<div class="clearfix"></div>
+	<div class="lines"></div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			
+		</div>
+	</div>
+	<div class="title-bg">
+		<div class="title">Alışveriş Sepetim</div>
+	</div>
+
+	<div class="table-responsive">
+		<table class="table table-bordered chart">
+			<thead>
+				<tr>
+					<th>Remove</th>
+					<th>Ürün Resim</th>
+					<th>Ürün ad</th>
+					<th>Ürün Kodu</th>
+					<th>Adet</th>
+					<th>Toplam Fiyat</th>
+				</tr>
+			</thead>
+			<tbody>
+
+
+				<?php 
+				$kullanici_id=$kullanicicek['kullanici_id'];
+				$sepetsor=$db->prepare("SELECT * FROM sepet where kullanici_id=:id");
+				$sepetsor->execute(array(
+					'id' => $kullanici_id
+					));
+
+				while($sepetcek=$sepetsor->fetch(PDO::FETCH_ASSOC)) {
+
+					$urun_id=$sepetcek['urun_id'];
+					$urunsor=$db->prepare("SELECT * FROM urun where urun_id=:urun_id");
+					$urunsor->execute(array(
+						'urun_id' => $urun_id
+						));
+
+					$uruncek=$urunsor->fetch(PDO::FETCH_ASSOC);
+
+					//echo $topla=$uruncek['urun_fiyat']*$sepetcek['urun_adet'];
+					?>
+					
+				<?php
+
+				if ($_GET['sepetsil']=="ok") {
+					$sil=$db->prepare("DELETE from sepet where sepet_id=:sepet_id");
+					$kontrol=$sil->execute(array(
+					'sepet_id' => $_GET['sepet_id']
+				));
+
+				if ($kontrol) {
+
+		
+					Header("Location:../eticarets/sepet.php?durum=ok");
+
+				} else {
+
+					Header("Location:../eticarets/sepet.php?durum=no");
+					}
+
+				}?>
+
+					<tr>
+						<td><center><a href="../eticarets/sepet.php?sepet_id=<?php echo $sepetcek['sepet_id']; ?>&sepetsil=ok"><button type="submit" name="sepetsil" class="btn btn-default btn-red btn-sm">Sil</button></a></center></td>
+
+
+						<?php
+								$urun_id=$uruncek["urun_id"];
+								$urunfoto=$db->prepare("SELECT * FROM urunfoto where urun_id=:urun_id");
+								$urunfoto->execute(array(
+									'urun_id' => $urun_id
+								));
+
+								$urunfotoce=$urunfoto->fetch(PDO::FETCH_ASSOC);
+
+								 ?>
+						<td><img src="<?php echo $urunfotoce['urunfoto_resimyol'] ?>" width="100" alt=""></td>
+						<td><?php echo $uruncek['urun_ad'] ?></td>
+						<td><?php echo $uruncek['urun_id'] ?></td>
+						<td><form><input type="text" class="form-control quantity" value="<?php echo $sepetcek['urun_adet'] ?>"></form></td>
+						<td><?php echo $uruncek['urun_fiyat'] ?></td>
+					</tr>
+					<?php } ?>
+
+				</tbody>
+			</table>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+
+
+			</div>
+			<div class="col-md-3 col-md-offset-3">
+				<div class="subtotal-wrap">
+					<!--<div class="subtotal">
+						<<p>Toplam Fiyat : $26.00</p>
+						<p>Vat 17% : $54.00</p>
+					</div>-->
+					<div class="total">Toplam Fiyat : <span class="bigprice"><?php echo $toplam_fiyat ?> TL</span></div>
+					<div class="clearfix"></div>
+					<a href="odeme" class="btn btn-default btn-yellow">Ödeme Sayfası</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+		<div class="spacer"></div>
+	</div>
+
+	<?php include 'footer.php' ?>
